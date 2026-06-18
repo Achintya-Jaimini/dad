2fed6c19ea60edb800d76d5c5946921c96998c10
Achintya Jaimini <ajaimini@ucdavis.edu>
2026-06-18 16:59:20 -0700

    made changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-SQXR1K6PSG");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
